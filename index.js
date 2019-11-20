225f1ef98395e9161a4aee14dae73caeaade43fe
Chintan Patel <chintan_patel818@hotmail.co.uk>
2019-11-20 10:04:52 +0000

    add heroku config

--- index.js
+++ index.js
@@ -3,9 +3,21 @@ const keys = require("./config/keys");
 
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("/hello", (req, res) => {
   res.send({ hello: "world!" });
 });
+
+// make express behave correctly in production environment
+if (process.env.NODE_ENV === "production") {
+  // serve static files from the React app
+  app.use(express.static("client/build"));
+
+  // tell express to serve up 'index.html' if it doesn't recognize the route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Listening @ ${PORT}`));
