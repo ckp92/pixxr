8c96f284c0dac866713812b41c5b0fd82ba53f6e
Chintan Patel <chintan_patel818@hotmail.co.uk>
2019-11-22 09:51:03 +0000

    confirm production db (on heroku) connects properly and comment out test code

--- index.js
+++ index.js
@@ -3,27 +3,27 @@ const mysql = require("mysql");
 const keys = require("./config/keys");
 const dbConfig = require("./config/dbConfig");
 
-const connection = mysql.createConnection(dbConfig.connection);
+// const connection = mysql.createConnection(dbConfig.connection);
 
-connection.connect(err => {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
+// connection.connect(err => {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
 
-  console.log("connected as id " + connection.threadId);
-});
+//   console.log("connected as id " + connection.threadId);
+// });
 
-const queries = ["SHOW DATABASES", "SELECT DATABASE()", "SHOW TABLES"];
+// const queries = ["SHOW DATABASES", "SELECT DATABASE()", "SHOW TABLES"];
 
-queries.forEach(query =>
-  connection.query(query, (err, results) => {
-    if (err) throw err;
-    console.log(results);
-  })
-);
+// queries.forEach(query =>
+//   connection.query(query, (err, results) => {
+//     if (err) throw err;
+//     console.log(results);
+//   })
+// );
 
-connection.end();
+// connection.end();
 
 const app = express();
 
