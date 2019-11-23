6c04bcdcdb9ea92686e85ddbec55af236cbd2c3e
Chintan Patel <chintan_patel818@hotmail.co.uk>
2019-11-23 19:40:33 +0000

    fix cookieKey typo

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,5 @@
 module.exports = {
-  cookieKEY: process.env.COOKIE_KEY,
+  cookieKey: process.env.COOKIE_KEY,
   rdsHost: process.env.RDS_HOST,
   rdsUser: process.env.RDS_USER,
   rdsPass: process.env.RDS_PASS,
